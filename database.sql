2e9521c7b44149d8ecce8948d5e77c382beed768
tschuitert <tom.schuitert@gmail.com>
2015-02-19 13:14:23 +0100

    DB Update

--- database.sql
+++ database.sql
@@ -4,14 +4,13 @@
 CREATE TABLE "album" (
 "id"  SERIAL ,
 "name_publisher" VARCHAR NOT NULL DEFAULT 'NULL' ,
-"id_artists" INTEGER ,
+"id_artist" INTEGER ,
 "name" VARCHAR NOT NULL DEFAULT 'NULL' ,
-"genre" VARCHAR ,
 "release_date" DATE ,
 PRIMARY KEY ("id")
 );
 
-CREATE TABLE "customers" (
+CREATE TABLE "customer" (
 "id"  SERIAL ,
 "firstname" VARCHAR ,
 "lastname" VARCHAR ,
@@ -19,7 +18,7 @@ CREATE TABLE "customers" (
 PRIMARY KEY ("id")
 );
 
-CREATE TABLE "artists" (
+CREATE TABLE "artist" (
 "id"  SERIAL ,
 "firstname" VARCHAR ,
 "lastname" VARCHAR ,
@@ -31,44 +30,39 @@ CREATE TABLE "publisher" (
 PRIMARY KEY ("name")
 );
 
-CREATE TABLE "tracks" (
+CREATE TABLE "track" (
 "id"  SERIAL ,
 "name" VARCHAR ,
 "duration" INTEGER ,
+"genre" VARCHAR ,
 PRIMARY KEY ("id")
 );
 
 CREATE TABLE "reservation" (
 "id"  SERIAL ,
-"id_customers" INTEGER ,
-"id_product" INTEGER ,
+"id_customer" INTEGER ,
+"id_movie" INTEGER ,
+"id_album" INTEGER ,
 PRIMARY KEY ("id")
 );
 
 CREATE TABLE "album_tracks" (
 "id_album" INTEGER ,
 "id_tracks" INTEGER ,
-"track_position" INTEGER ,
-PRIMARY KEY ()
-);
-
-CREATE TABLE "album_artists" (
-"id_album" INTEGER ,
-"id_artists" INTEGER ,
-PRIMARY KEY ()
+"track_position" INTEGER 
 );
 
 CREATE TABLE "tracks_artists" (
 "id_tracks" INTEGER ,
-"id_artists" INTEGER ,
-PRIMARY KEY ()
+"id_artists" INTEGER 
 );
 
 CREATE TABLE "product" (
 "id"  SERIAL ,
 "id_album" INTEGER ,
 "id_movie" INTEGER ,
-"stock" INTEGER ,
+"id_customer" INTEGER ,
+"expected_return" DATE ,
 PRIMARY KEY ("id")
 );
 
@@ -82,33 +76,32 @@ PRIMARY KEY ("id")
 
 CREATE TABLE "movie_artists" (
 "id_movie" INTEGER ,
-"id_artists" INTEGER ,
-PRIMARY KEY ()
+"id_artists" INTEGER 
 );
 
-CREATE TABLE "rentals" (
+CREATE TABLE "rental_history" (
 "id"  SERIAL ,
 "id_product" INTEGER ,
-"id_customers" INTEGER ,
+"id_customer" INTEGER ,
 "return_date" DATE ,
 PRIMARY KEY ("id")
 );
 
 ALTER TABLE "album" ADD FOREIGN KEY ("name_publisher") REFERENCES "publisher" ("name");
-ALTER TABLE "album" ADD FOREIGN KEY ("id_artists") REFERENCES "artists" ("id");
-ALTER TABLE "reservation" ADD FOREIGN KEY ("id_customers") REFERENCES "customers" ("id");
-ALTER TABLE "reservation" ADD FOREIGN KEY ("id_product") REFERENCES "product" ("id");
+ALTER TABLE "album" ADD FOREIGN KEY ("id_artist") REFERENCES "artist" ("id");
+ALTER TABLE "reservation" ADD FOREIGN KEY ("id_customer") REFERENCES "customer" ("id");
+ALTER TABLE "reservation" ADD FOREIGN KEY ("id_movie") REFERENCES "movie" ("id");
+ALTER TABLE "reservation" ADD FOREIGN KEY ("id_album") REFERENCES "album" ("id");
 ALTER TABLE "album_tracks" ADD FOREIGN KEY ("id_album") REFERENCES "album" ("id");
-ALTER TABLE "album_tracks" ADD FOREIGN KEY ("id_tracks") REFERENCES "tracks" ("id");
-ALTER TABLE "album_artists" ADD FOREIGN KEY ("id_album") REFERENCES "album" ("id");
-ALTER TABLE "album_artists" ADD FOREIGN KEY ("id_artists") REFERENCES "artists" ("id");
-ALTER TABLE "tracks_artists" ADD FOREIGN KEY ("id_tracks") REFERENCES "tracks" ("id");
-ALTER TABLE "tracks_artists" ADD FOREIGN KEY ("id_artists") REFERENCES "artists" ("id");
+ALTER TABLE "album_tracks" ADD FOREIGN KEY ("id_tracks") REFERENCES "track" ("id");
+ALTER TABLE "tracks_artists" ADD FOREIGN KEY ("id_tracks") REFERENCES "track" ("id");
+ALTER TABLE "tracks_artists" ADD FOREIGN KEY ("id_artists") REFERENCES "artist" ("id");
 ALTER TABLE "product" ADD FOREIGN KEY ("id_album") REFERENCES "album" ("id");
 ALTER TABLE "product" ADD FOREIGN KEY ("id_movie") REFERENCES "movie" ("id");
+ALTER TABLE "product" ADD FOREIGN KEY ("id_customer") REFERENCES "customer" ("id");
 ALTER TABLE "movie" ADD FOREIGN KEY ("name_publisher") REFERENCES "publisher" ("name");
 ALTER TABLE "movie_artists" ADD FOREIGN KEY ("id_movie") REFERENCES "movie" ("id");
-ALTER TABLE "movie_artists" ADD FOREIGN KEY ("id_artists") REFERENCES "artists" ("id");
-ALTER TABLE "rentals" ADD FOREIGN KEY ("id_product") REFERENCES "product" ("id");
-ALTER TABLE "rentals" ADD FOREIGN KEY ("id_customers") REFERENCES "customers" ("id");
+ALTER TABLE "movie_artists" ADD FOREIGN KEY ("id_artists") REFERENCES "artist" ("id");
+ALTER TABLE "rental_history" ADD FOREIGN KEY ("id_product") REFERENCES "product" ("id");
+ALTER TABLE "rental_history" ADD FOREIGN KEY ("id_customer") REFERENCES "customer" ("id");
 
